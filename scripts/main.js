ad82abe27393f04b14c90bf3fc5673538885e374
Chris Cresswell <christorper@gmail.com>
2021-06-17 00:05:52 -0700

    Add change user button and welcome message

--- scripts/main.js
+++ scripts/main.js
@@ -12,3 +12,38 @@ myImage.onclick = function()
         myImage.setAttribute('src', 'https://d33wubrfki0l68.cloudfront.net/06185f059f69055733688518b798a0feb4c7f160/9f07a/images/product-identity-assets/firefox.png')
     }
 }
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() 
+{
+    let myName = prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Mozilla is cool, ' + myName;
+    
+    if(!myName)
+    {
+        setUserName();
+    }
+    else
+    {
+        localStorage.setItem('name', myName);
+        myHeading.textContent = 'Mozilla is cool, ' + myName;
+    }
+
+    if(!localStorage.getItem('name'))
+    {
+        setUserName();
+    }
+    else
+    {
+        let storedName = localStorage.getItem('name');
+        myHeading.textContent = 'Mozilla is cool, ' + storedName;
+    }
+}
+
+myButton.onclick = function()
+{
+    setUserName();
+}
